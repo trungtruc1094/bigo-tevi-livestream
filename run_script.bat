be764d637d24e02cd1228bb91c35a1171df10e61
trucnguyen-pc4994 <truc.nguyen4994@gmail.com>
2024-09-25 11:30:01 +0700

    sd

--- run_script.bat
+++ run_script.bat
@@ -4,7 +4,7 @@ echo Killing all Chrome processes...
 taskkill /F /IM chrome.exe /T
 
 REM Step 2: Launch Chrome with remote debugging port
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9223 --user-data-dir="C:\Users\trung\AppData\Local\Google\Chrome\User Data\Profile 32"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9223 --user-data-dir="C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default"
 
 REM Step 3: Wait for manual setup of the livestream session
 echo "Please set up your livestream session on the recipient platform, then provide the necessary information."
